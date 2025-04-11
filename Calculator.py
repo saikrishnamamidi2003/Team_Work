0d462e4e315039167de1bdf25e8880775cc64aba
Rangasai <rangasaimangalagiri@gmail.com>
2025-04-11 11:50:38 +0530

    Initail code comitted

---
+++ Calculator.py
@@ -0,0 +1,34 @@
+import math
+
+def get_input():
+    """Function to take input from the user."""
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    return a, b
+
+def add(a, b):
+    pass
+
+def subtract(a, b):
+    pass
+
+def multiply(a, b):
+    pass
+
+def divide(a, b):
+    pass
+
+
+def modulus(a, b):
+    pass
+
+
+def calculator():
+    print("\nWelcome to the Team Calculator!")
+    a, b = get_input()  # Taking input for `a` and `b`
+    print(f"Values received: a = {a}, b = {b}")
+    
+    
+
+if __name__ == "__main__":
+    calculator()
